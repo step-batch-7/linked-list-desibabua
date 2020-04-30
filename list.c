15270b4c329c7adc4e551ffb9bd28c67349c72be
ayush <58028488+desibabua@users.noreply.github.com>
2020-04-30 16:23:52 +0530

    added a function to add value in end and display a list

--- list.c
+++ list.c
@@ -1,2 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "list.h"
 
+Node_ptr create_node(int value) {
+  Node_ptr node = malloc(sizeof(Node));
+  node->value = value;
+  node->next = NULL;
+  return node;
+}
+
+List_ptr create_list(void){
+  List_ptr list = malloc(sizeof(List));
+  list->head = NULL;
+  list->last = NULL;
+  list->count = 0;
+  return list;
+}
+
+Status add_to_end(List_ptr list, int value){
+  Node_ptr node = create_node(value);
+  if (list->head == NULL)
+  {
+    list->head = node;
+  } else
+  {
+    list->last->next = node;
+  }
+  list->last = node;
+  Status status = Success;
+  return status;
+}
+
+void display(List_ptr list) {
+  Node_ptr p_walk = list->head;
+  while (p_walk != NULL)
+  {
+    printf("%d ", p_walk->value);
+    p_walk = p_walk->next;
+  }
+}
--- numbers.c
+++ numbers.c
@@ -3,6 +3,12 @@
 
 int main(void)
 {
-  printf("hello\n");
+  List_ptr list = create_list();
+  add_to_end(list, 5);
+  add_to_end(list, 6);
+  add_to_end(list, 4);
+  add_to_end(list, 3);
+
+  display(list);
   return 0;
 }
